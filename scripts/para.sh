77fac822662234510d219e86d7dc4bd9d70564f5
unixextremist <ysfmmekb@proton.me>
2026-02-23 01:07:04 +0400

    Create para.sh

---
+++ scripts/para.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+command -v wmctrl >/dev/null 2>&1 || exit 1
+
+workspaces=$(
+    wmctrl -d | awk '
+        {
+            name = $NF
+            if (name == "-" || name == "") name = "ws" $1
+            if ($2 == "*") name = "[" name "]"
+            printf "%s%s", sep, name
+            sep = " / "
+        }
+    '
+)
+
+[ -z "$workspaces" ] && exit 1
+
+if command -v notify-send >/dev/null 2>&1; then
+    notify-send "workspaces" "$workspaces"
+elif command -v herbe >/dev/null 2>&1; then
+    herbe "workspaces: $workspaces"
+else
+    printf '%s\n' "$workspaces"
+fi
